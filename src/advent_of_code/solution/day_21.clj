73bd523cfd8fe2bd7e7f834270e79828d9bcf077
Ben Grabow <ben.grabow@singlewire.com>
2020-12-21 12:36:26 -0600

    Solve day-21 part-1

---
+++ src/advent_of_code/solution/day_21.clj
@@ -0,0 +1,29 @@
+(ns advent-of-code.solution.day-21
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(let [foods (->> (slurp "resources/input/day-21.txt")
+                 (str/split-lines)
+                 (map #(->> (str/split % #"\(contains")
+                            (map (partial re-seq #"\w+")))))]
+  (->> (for [[ingredients allergens] foods]
+         (into {} (map #(vector % (set ingredients)) allergens)))
+       (apply merge-with set/intersection)))
+
+(def part-1
+  (let [allergen-translations {"wheat" #{"kqms"},
+                               "eggs" #{"bjq"},
+                               "nuts" #{"klplr"},
+                               "shellfish" #{"tlgjzx"},
+                               "peanuts" #{"dtvhzt"},
+                               "sesame" #{"sbzd"},
+                               "soy" #{"ctmbr"},
+                               "fish" #{"jznhvh"}}
+        foods (->> (slurp "resources/input/day-21.txt")
+                   (str/split-lines)
+                   (map #(->> (str/split % #"\(contains")
+                              (map (partial re-seq #"\w+")))))
+        allergic-ingredients (set (for [[_ ingredients] allergen-translations]
+                                    (first (vec ingredients))))]
+    (reduce + (for [[ingredients] foods]
+                (count (set/difference (set ingredients) allergic-ingredients))))))
