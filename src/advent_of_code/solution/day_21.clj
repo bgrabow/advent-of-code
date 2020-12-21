d604755dea1a0f960ab41e581b08ad141b082019
Ben Grabow <ben.grabow@singlewire.com>
2020-12-21 13:12:23 -0600

    Automate winnowing allergen translations

--- src/advent_of_code/solution/day_21.clj
+++ src/advent_of_code/solution/day_21.clj
@@ -2,42 +2,49 @@
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-(let [foods (->> (slurp "resources/input/day-21.txt")
-                 (str/split-lines)
-                 (map #(->> (str/split % #"\(contains")
-                            (map (partial re-seq #"\w+")))))]
-  (->> (for [[ingredients allergens] foods]
-         (into {} (map #(vector % (set ingredients)) allergens)))
-       (apply merge-with set/intersection)))
+(defn winnow-possibilities
+  [m]
+  (->> (iterate #(let [decided-possibility (first
+                                             (keep
+                                               (fn [[_a possibilities]]
+                                                 (when (= 1 (count possibilities))
+                                                   (first (seq possibilities))))
+                                               %))]
+                   (map (fn [[a possibilities]]
+                          [a (disj (set possibilities) decided-possibility)])
+                        %))
+                (seq m))
+       (map (fn [m]
+              (first
+                (keep
+                  (fn [[a possibilities]]
+                    (when (= 1 (count possibilities))
+                      [a (first (seq possibilities))]))
+                  m))))
+       (take-while some?)
+       (into {})))
 
-(def part-1
-  (let [allergen-translations {"wheat" #{"kqms"},
-                               "eggs" #{"bjq"},
-                               "nuts" #{"klplr"},
-                               "shellfish" #{"tlgjzx"},
-                               "peanuts" #{"dtvhzt"},
-                               "sesame" #{"sbzd"},
-                               "soy" #{"ctmbr"},
-                               "fish" #{"jznhvh"}}
-        foods (->> (slurp "resources/input/day-21.txt")
+(def allergen-translations
+  (let [foods (->> (slurp "resources/input/day-21.txt")
                    (str/split-lines)
                    (map #(->> (str/split % #"\(contains")
                               (map (partial re-seq #"\w+")))))
-        allergic-ingredients (set (for [[_ ingredients] allergen-translations]
-                                    (first (vec ingredients))))]
+        possible-allergens (->> (for [[ingredients allergens] foods]
+                                  (into {} (map #(vector % (set ingredients)) allergens)))
+                                (apply merge-with set/intersection))]
+    (winnow-possibilities possible-allergens)))
+
+(def part-1
+  (let [foods (->> (slurp "resources/input/day-21.txt")
+                   (str/split-lines)
+                   (map #(->> (str/split % #"\(contains")
+                              (map (partial re-seq #"\w+")))))
+        allergic-ingredients (set (map second allergen-translations))]
     (reduce + (for [[ingredients] foods]
                 (count (set/difference (set ingredients) allergic-ingredients))))))
 
 
 (def part-2
-  (let [sorted-allergens (sort-by first {"wheat" #{"kqms"},
-                                         "eggs" #{"bjq"},
-                                         "nuts" #{"klplr"},
-                                         "shellfish" #{"tlgjzx"},
-                                         "peanuts" #{"dtvhzt"},
-                                         "sesame" #{"sbzd"},
-                                         "soy" #{"ctmbr"},
-                                         "fish" #{"jznhvh"}})]
+  (let [sorted-allergens (sort-by first allergen-translations)]
     (->> (map second sorted-allergens)
-         (map #(first (vec %)))
          (str/join ","))))
