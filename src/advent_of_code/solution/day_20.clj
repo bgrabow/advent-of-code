138585fdad34ea8dc9caf973e30600292ffa62a3
Ben Grabow <ben.grabow@gmail.com>
2020-12-20 21:44:11 -0600

    Create edge-index for part-2

--- src/advent_of_code/solution/day_20.clj
+++ src/advent_of_code/solution/day_20.clj
@@ -20,7 +20,7 @@
    (mapv vector (range 10) (repeat 9))])
 
 (def part-1
-  (let [tiles (->> (map parse-tile (str/split (slurp "resources/input/day-20.txt") #"\n\n")))
+  (let [tiles (map parse-tile (str/split (slurp "resources/input/day-20.txt") #"\n\n"))
         tile-edges (->> tiles
                         (map (fn [[id tile]]
                                [id (mapcat #(vector
@@ -36,9 +36,42 @@
                          (keep (fn [[edge f]]
                                  (when (= 1 f)
                                    edge)))
-                         set)]
-    (->> (keep (fn [[id edges]]
-                 (when (= 4 (count (filter outer-edges edges)))
-                   id))
-               tile-edges)
-         (reduce *))))
+                         set)
+        corner-ids (->> (keep (fn [[id edges]]
+                                (when (= 4 (count (filter outer-edges edges)))
+                                  id))
+                              tile-edges))]
+    (reduce * corner-ids)))
+
+(defn rotate-right
+  [m]
+  (into {}
+        (for [[[x y] c] m]
+          [[(- 9 y) x] c])))
+
+(defn rotations
+  [contents]
+  (take 4 (iterate rotate-right contents)))
+
+(defn flip
+  [contents]
+  (into {} (for [[[x y] c] (vec contents)]
+             [[(- 9 x) y] c])))
+
+#_(rotate-right (second (first (map parse-tile (str/split (slurp "resources/input/day-20.txt") #"\n\n")))))
+
+(let [tiles (map parse-tile (str/split (slurp "resources/input/day-20.txt") #"\n\n"))
+      edge-index (->> (mapcat (fn [[_id contents]]
+                                (for [contents (concat (rotations contents)
+                                                       (rotations (flip contents)))]
+                                  {:left {(mapv contents (edges 0)) contents}
+                                   :right {(mapv contents (edges 1)) contents}
+                                   :top {(mapv contents (edges 2)) contents}
+                                   :bottom {(mapv contents (edges 2)) contents}}))
+                              tiles)
+                      (apply merge-with merge))])
+
+;; Choose first corner.
+;; Find rotation for top left.
+;; Iterate down column finding next tiles.
+;; For each row, iterate across row finding next tiles.
