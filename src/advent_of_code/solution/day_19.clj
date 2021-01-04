6d9a82b7d0c7c3ca260ab6c8850878131b9afe5a
Ben Grabow <ben.grabow@gmail.com>
2021-01-03 20:46:45 -0600

    Solve d19p1 by hand instead of with library.

--- src/advent_of_code/solution/day_19.clj
+++ src/advent_of_code/solution/day_19.clj
@@ -1,13 +1,8 @@
 (ns advent-of-code.solution.day-19
   (:require [clojure.string :as str]
-            [instaparse.core :as insta])
+            [instaparse.core :as insta]
+            [clojure.walk :as walk])
   (:import (instaparse.gll Failure)))
-
-(defn valid?
-  ([rules s]
-   (valid? rules s 0))
-  ([rules _s rule]
-   (if (string? (rules rule)))))
 
 (def part-1
   (let [[rule-strings [_ & messages]] (split-with (complement str/blank?)
@@ -23,7 +18,7 @@
                                               (re-seq #"\d+" s)))
                                        (str/split contents #"\|")))])))
                     (into {}))
-         parser (insta/parser (str/join "\n" (cons "S: 0" rule-strings)))]
+        parser (insta/parser (str/join "\n" (cons "S: 0" rule-strings)))]
     (->> (map parser messages)
          (remove #(instance? Failure %))
          count)))
@@ -46,3 +41,89 @@
     (->> (map parser messages)
          (remove #(instance? Failure %))
          count)))
+
+(defn parse-rule
+  [s]
+  (let [[rule-number matches] (str/split s #"\:")
+        alternations (set (map #(vec (re-seq #"\b(?:\d+|\w)\b" %)) (str/split matches #"\|")))]
+    [rule-number alternations]))
+
+(def lowercase-letters (set (map char (range (int \a) (inc (int \z))))))
+
+(defn simplified?
+  [alternations]
+  (not (re-find #"\d" (str alternations))))
+
+(defn simplify
+  ([{:keys [simplified unsimplified]}]
+   (let [substituted (map (fn [[rule-number alternations]]
+                            [rule-number (walk/postwalk #(or (simplified %) %)
+                                                        alternations)])
+                          unsimplified)]
+     {:simplified (into simplified (filter simplified? substituted))
+      :unsimplified (remove simplified? substituted)}))
+  ([root rules]
+   (walk/prewalk #(or (get rules %) %) (get rules root))))
+
+(defn iterate-until-fixed
+  [f x]
+  (reduce (fn [s x]
+            (if (s x) (reduced x) (conj s x)))
+          #{} (iterate f x)))
+
+(defn nested-coll?
+  [coll]
+  (and (seq coll)
+       (some coll? coll)))
+
+(defn concat-sets
+  [node]
+  (and (vector? node)
+       (every? (fn [x]
+                 (or (string? x)
+                     (and (set? x)
+                          (not (nested-coll? x)))))
+               node)
+       (set (reduce (fn [ss x]
+                      (if (set? x)
+                        (mapcat (fn [s]
+                                  (for [x x]
+                                    (str s x)))
+                                ss)
+                        (for [s ss]
+                          (str s x))))
+                    [""] node))))
+
+(defn combine-nested-sets
+  [node]
+  (and (set? node)
+       (every? (fn [x]
+                 (or (string? x)
+                     (set? x))) node)
+       (reduce (fn [s x]
+                 (if (set? x)
+                   (into s x)
+                   (conj s x)))
+               #{} node)))
+
+(defn reduce-nesting
+  [t]
+  (walk/postwalk #(or (and (vector? %)
+                         (every? string? %)
+                         (str/join %))
+                      (and (or (set? %) (vector? %) (list? %))
+                           (= 1 (count %))
+                           (first %))
+                      (concat-sets %)
+                      (combine-nested-sets %)
+                      %)
+                 t))
+
+(def part-2-not-invented-here
+  (let [[rules [_ & messages]] (split-with (complement str/blank?)
+                                           (str/split-lines (slurp "resources/input/day-19.txt")))
+        compiled-rule (->> (map parse-rule rules)
+                           (into {})
+                           (simplify "0")
+                           (iterate-until-fixed reduce-nesting))]
+    (count (filter compiled-rule messages))))
